252c5b4d0cbef3dd62a6c4244a595a95264ffb80
afentev <kirill.afentyev@yandex.ru>
2022-09-21 23:06:22 +0300

    sum hw

--- CAOS 2.0/1. Numbers/alu-sum.c
+++ CAOS 2.0/1. Numbers/alu-sum.c
@@ -3,8 +3,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define ITYPE uint32_t
-
 void sum(ITYPE first, ITYPE second, ITYPE* res, int* CF)
 {
   *res = 0;
@@ -16,26 +14,4 @@ void sum(ITYPE first, ITYPE second, ITYPE* res, int* CF)
     first >>= 1;
     second >>= 1;
   }
-}
-
-void mul(ITYPE first, ITYPE second, ITYPE* res, int* CF)
-{
-    *res = 0;
-    *CF = 0;
-
-    for (uint32_t bit = 0; bit < sizeof(ITYPE) * CHAR_BIT; ++bit) {
-        int overflow = *CF;
-        sum(((first & 1) * second) << bit, *res, res, CF);
-        *CF |= overflow;
-        first >>= 1;
-    }
-}
-
-int main() {
-  ITYPE* res = malloc(sizeof(ITYPE));
-  int* cf = malloc(sizeof(int));
-  ITYPE first = 5;
-  ITYPE second = 2;
-  mul(first, second, res, cf);
-  printf("%u %i\n", *res, *cf);
 }
