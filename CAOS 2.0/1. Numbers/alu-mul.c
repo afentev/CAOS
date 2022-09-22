257f7c5e28fca276dc8ecdf44c28dce7d117c9c8
afentev <kirill.afentyev@yandex.ru>
2022-09-22 23:53:13 +0300

    hw #1 fix

--- CAOS 2.0/1. Numbers/alu-mul.c
+++ CAOS 2.0/1. Numbers/alu-mul.c
@@ -3,11 +3,13 @@
 
 void sum(ITYPE first, ITYPE second, ITYPE* res, int* CF)
 {
+    *res = 0;
     *CF = 0;
     for (uint32_t bit = 0; bit < sizeof(ITYPE) * CHAR_BIT; ++bit) {
-        ITYPE sum = (first & 1) + (second & 1) + *CF;
-        *CF = (sum > 1);
-        *res |= ((sum & 1) << bit);
+        ITYPE left = first & 1;
+        ITYPE right = second & 1;
+        *res |= (left ^ right ^ *CF) << bit;
+        *CF = (int)((left & right) | (left & *CF) | (right & *CF));
         first >>= 1;
         second >>= 1;
     }
@@ -19,7 +21,9 @@ void mul(ITYPE first, ITYPE second, ITYPE* res, int* CF)
     *CF = 0;
     for (uint32_t bit = 0; bit < sizeof(ITYPE) * CHAR_BIT; ++bit) {
         int overflow = *CF;
-        sum(((first & 1) * second) << bit, *res, res, CF);
+        ITYPE mask = ~(ITYPE)(0);
+        sum(mask, first & 1, &mask, CF);
+        sum((~mask & second) << bit, *res, res, CF);
         *CF |= overflow;
         first >>= 1;
     }
--- CAOS 2.0/1. Numbers/alu-sum.c
+++ CAOS 2.0/1. Numbers/alu-sum.c
@@ -3,15 +3,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define ITYPE uint32_t
+
 void sum(ITYPE first, ITYPE second, ITYPE* res, int* CF)
 {
   *res = 0;
   *CF = 0;
   for (uint32_t bit = 0; bit < sizeof(ITYPE) * CHAR_BIT; ++bit) {
-    ITYPE sum = (first & 1) + (second & 1) + *CF;
-    *CF = (sum > 1);
-    *res |= ((sum & 1) << bit);
+    ITYPE left = first & 1;
+    ITYPE right = second & 1;
+    *res |= (left ^ right ^ *CF) << bit;
+    *CF = (int)((left & right) | (left & *CF) | (right & *CF));
     first >>= 1;
     second >>= 1;
   }
+}
+
+int main() {
+  ITYPE first = 13;
+  ITYPE second = 15;
+  ITYPE* res = malloc(sizeof(ITYPE));
+  int* CF = malloc(sizeof(int));
+  sum(first, second, res, CF);
+  printf("%d %d", *res, *CF);
 }
