82c51f955af5953ca3d74a047ff8f4d9139ef7bf
afentev <kirill.afentyev@yandex.ru>
2022-09-24 01:20:02 +0300

    hw #1 utf8 done

--- CAOS 2.0/1. Numbers/utf8-count-symbols.c
+++ CAOS 2.0/1. Numbers/utf8-count-symbols.c
@@ -1,14 +1,53 @@
+#include <stdint.h>
 #include <stdio.h>
+
+uint64_t isAllZeros(uint64_t n)
+{
+    n |= n >> 32;
+    n |= n >> 16;
+    n |= n >> 8;
+    n |= n >> 4;
+    n |= n >> 2;
+    n |= n >> 1;
+    return (n & 1) ^ 1;
+}
 
 int main()
 {
-  char buffer[10];
-  int max_len = (int)sizeof(buffer) - 1, actual_len = 0;
-  char format[32]; // Гарантированно достаточный буффер для генерируемой форматной строки.
-  snprintf(format, sizeof(format), "%%%ds%%n", (int)max_len);
-  if (sscanf("aboba\nk", format, buffer, &actual_len) == 1 && actual_len != max_len) {
-    printf("complete read: %s\n", buffer);
-  } else {
-    printf("incomplete read: %s\n", buffer);
-  }
+    uint32_t c;
+    int ascii = 0;
+    int nonascii = 0;
+    int skip = 0;
+    while ((c = getchar()) ^ EOF) {
+        if (skip) {
+            if ((c >> 6) ^ 0b10) {
+                printf("%d %d", ascii, nonascii - 1);
+                return 1;
+            }
+            skip >>= 1;
+            continue;
+        }
+        int isUnicode = c >> 7;
+        if (isUnicode) {
+            if ((c >> 6) ^ 0b11 | isAllZeros(c ^ 0xFF)) {
+                printf("%d %d", ascii, nonascii);
+                return 1;
+            }
+            int count = __builtin_clz((~c & 0xFFu) << 26);
+            if (count > 2) {
+                printf("%d %d", ascii, nonascii);
+                return 1;
+            }
+            nonascii++;
+            skip = 1 << count;
+        } else {
+            ascii++;
+        }
+    }
+    if (skip ^ 0) {
+        printf("%d %d", ascii, nonascii - 1);
+        return 1;
+    }
+    printf("%d %d", ascii, nonascii);
+    return 0;
 }
