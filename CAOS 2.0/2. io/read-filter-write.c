593d9e0b64ac6bc71cf40ddafc0b973c1f9cd48f
afentev <kirill.afentyev@yandex.ru>
2022-09-27 20:52:10 +0300

    hw #2 problem 1 fix

--- CAOS 2.0/2. io/read-filter-write.c
+++ CAOS 2.0/2. io/read-filter-write.c
@@ -3,8 +3,29 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define BAD_ERRNO(condition)                                                   \
+int close_file(int fd)
+{
+    if (fd != -1) {
+        while (close(fd) < 0) {
+            if (errno != EINTR) {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int close_files(int fd1, int fd2, int fd3)
+{
+    int error = close_file(fd1);
+    error |= close_file(fd2);
+    error |= close_file(fd3);
+    return error;
+}
+
+#define BAD_ERRNO(condition, fd1, fd2, fd3)                                    \
     if (condition) {                                                           \
+        close_files(fd1, fd2, fd3);                                            \
         return 1;                                                              \
     }
 
@@ -16,13 +37,13 @@ int main(int argc, char** argv)
     int input_d, output_dig_d, output_nondig_d;
 
     while ((input_d = open(argv[1], O_RDONLY)) < 0) {
-        BAD_ERRNO(errno != EINTR);
+        BAD_ERRNO(errno != EINTR, -1, -1, -1);
     }
     while ((output_dig_d = open(argv[2], O_WRONLY | O_CREAT, 0640)) < 0) {
-        BAD_ERRNO(errno != EINTR);
+        BAD_ERRNO(errno != EINTR, input_d, -1, -1);
     }
     while ((output_nondig_d = open(argv[3], O_WRONLY | O_CREAT, 0640)) < 0) {
-        BAD_ERRNO(errno != EINTR);
+        BAD_ERRNO(errno != EINTR, input_d, output_dig_d, -1);
     }
 
     char buff[BUFF_SIZE];
@@ -31,7 +52,11 @@ int main(int argc, char** argv)
     int read_chr;
     while ((read_chr = read(input_d, buff, BUFF_SIZE))) {
         if (read_chr == -1) {
-            BAD_ERRNO(errno != EAGAIN && errno != EINTR);
+            BAD_ERRNO(
+                errno != EAGAIN && errno != EINTR,
+                input_d,
+                output_dig_d,
+                output_nondig_d);
             continue;
         }
         int digits_cnt = 0;
@@ -44,21 +69,19 @@ int main(int argc, char** argv)
             }
         }
         while (write(output_dig_d, digits_buff, digits_cnt) < 0) {
-            BAD_ERRNO(errno != EAGAIN && errno != EINTR);
+            BAD_ERRNO(
+                errno != EAGAIN && errno != EINTR,
+                input_d,
+                output_dig_d,
+                output_nondig_d);
         }
         while (write(output_nondig_d, non_digits_buff, non_digits_cnt) < 0) {
-            BAD_ERRNO(errno != EAGAIN && errno != EINTR);
+            BAD_ERRNO(
+                errno != EAGAIN && errno != EINTR,
+                input_d,
+                output_dig_d,
+                output_nondig_d);
         }
     }
-
-    while (close(input_d) < 0) {
-        BAD_ERRNO(errno != EINTR);
-    }
-    while (close(output_dig_d) < 0) {
-        BAD_ERRNO(errno != EINTR);
-    }
-    while (close(output_nondig_d) < 0) {
-        BAD_ERRNO(errno != EINTR);
-    }
-    return 0;
+    return close_files(input_d, output_dig_d, output_nondig_d);
 }
