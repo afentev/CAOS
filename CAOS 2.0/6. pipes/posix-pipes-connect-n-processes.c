6b18c1df07623d49f59191e160ba811e87cfae8c
afentev <kirill.afentyev@yandex.ru>
2022-10-28 16:02:48 +0300

    hw #6 problem 3 improved

--- CAOS 2.0/6. pipes/posix-pipes-connect-n-processes.c
+++ CAOS 2.0/6. pipes/posix-pipes-connect-n-processes.c
@@ -5,25 +5,20 @@
 int main(int argc, char* argv[])
 {
     assert(argc >= 3);
-    int fd[2][2];
-    pipe(fd[0]);
-    pipe(fd[1]);
-    int cur = 0;
+    int fd[2];
     for (int i = 0; i + 2 < argc; ++i) {
+        pipe(fd);
         pid_t pid = fork();
         if (pid == 0) {
-            close(fd[cur][0]);
-            dup2(fd[cur][1], STDOUT_FILENO);
-            close(fd[cur][1]);
+            close(fd[0]);
+            dup2(fd[1], STDOUT_FILENO);
+            close(fd[1]);
             execlp(argv[i + 1], argv[i + 1], NULL);
             return 1;
         }
-        close(fd[cur][1]);
-        dup2(fd[cur][0], STDIN_FILENO);
-        close(fd[cur][0]);
-
-        pipe(fd[cur]);
-        cur = (cur + 1) % 2;
+        close(fd[1]);
+        dup2(fd[0], STDIN_FILENO);
+        close(fd[0]);
     }
     execlp(argv[argc - 1], argv[argc - 1], NULL);
 }
